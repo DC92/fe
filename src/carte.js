7b23fbf98697d083e6866b42cf5e9399d807b988
Dominique Cavailhez <rando.gv92@gmail.com>
2026-01-02 16:21:42 +0100

    On sort la déclaration de layer wriPoi

--- src/carte.js
+++ src/carte.js
@@ -37,6 +37,39 @@ const baseLayers = {
   }),
 };
 
+const wriPoiLayer =
+  L.geoJson(null, {
+    // Icônes WRI
+    pointToLayer: (feature, latlng) => L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: serveurApi + '/images/icones/' + feature.properties.type.icone + '.svg',
+        iconSize: [24, 24],
+        iconAnchor: [12, 12],
+      })
+    }),
+
+    onEachFeature: (feature, layer) => {
+      // Etiquette sur les points
+      layer.bindTooltip(
+        feature.properties.nom, {
+          direction: 'center',
+          offset: L.point(0, -24),
+        }
+      ).openTooltip();
+
+      // Clic
+      layer.on({
+        click: () => {
+          // Affiche les donnés d'entête de la fiche qui sont disponibles dans l'API bbox
+          appliqueDonnees('point', feature.properties);
+
+          // Affiche la page point
+          window.location.hash = 'point=' + feature.properties.id;
+        },
+      });
+    }
+  });
+
 /* eslint-disable-next-line no-unused-vars */
 function initCarte() {
   if (!map) {
@@ -60,42 +93,14 @@ function initCarte() {
 
     L.Permalink.setup(map); //TODO BUG Interférence permalink templateur
 
+    // Refuges.info points of interest
+    wriPoiLayer.addTo(map);
     requeteAPI(
       'cartes',
       '/api/bbox?&nb_points=all&bbox=4.8%2C44.5%2C7.4%2C46.2', // French north Apls
       null,
       json => {
-        L.geoJson(json, {
-          // Icônes WRI
-          pointToLayer: (feature, latlng) => L.marker(latlng, {
-            icon: L.icon({
-              iconUrl: serveurApi + '/images/icones/' + feature.properties.type.icone + '.svg',
-              iconSize: [24, 24],
-              iconAnchor: [12, 12],
-            })
-          }),
-
-          onEachFeature: (feature, layer) => {
-            // Etiquette sur les points
-            layer.bindTooltip(
-              feature.properties.nom, {
-                direction: 'center',
-                offset: L.point(0, -24),
-              }
-            ).openTooltip();
-
-            // Clic
-            layer.on({
-              click: () => {
-                // Affiche les donnés d'entête de la fiche qui sont disponibles dans l'API bbox
-                appliqueDonnees('point', feature.properties);
-
-                // Affiche la page point
-                window.location.hash = 'point=' + feature.properties.id;
-              },
-            });
-          }
-        }).addTo(map);
+        wriPoiLayer.addData(json);
       }
     );
   }
