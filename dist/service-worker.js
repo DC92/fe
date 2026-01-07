3ca134a737cc9e5b0a2f3409cd2010cae19b1074
Dominique Cavailhez <rando.gv92@gmail.com>
2026-01-07 20:47:38 +0100

    OK install sauf fond carte

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,36 +1,24 @@
-/* PWA service worker
- * Instantiates in a context different from the HTML DOM
- */
+/* PWA service worker */
+//https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Caching
 //https://korben.info/pwa-cache-cauchemar-solution.html
 
-const cacheName = 'myWRI';
+const cacheName = 'refuges.info';
 
-// First event after installing the service worker
-self.addEventListener('install', evt => {
-  console.info('PWA install');
+// Fetch any ressource, cache first with cache refresh
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise =
+    fetch(request)
+    .then(async (networkResponse) => {
+      if (networkResponse.ok) {
+        const cache = await caches.open(cacheName);
+        cache.put(request, networkResponse.clone());
+      }
+      return networkResponse;
+    });
 
-  // Necessary for automatic files add, immediately activate the SW & trigger controllerchange
-  self.skipWaiting();
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
 
-  // Open/install cache
-  evt.waitUntil(
-    caches.open(cacheName)
-    .catch(error => 'Error opening cache ' + cacheName + ': ' + console.error(error))
-  );
-});
-
-// Provides the required files
-// Cache first, then browser cache, then network
-self.addEventListener('fetch', evt => {
-  evt.waitUntil(
-    caches.open(cacheName)
-    .then(() => console.info('PWA add ' + evt.request.url))
-    .catch(error => 'Error adding ' + evt.request.url + ': ' + console.error(error))
-  );
-
-  evt.respondWith(
-    caches.match(evt.request)
-    .then(found => found || fetch(evt.request))
-    .catch(error => 'Error matching ' + evt.request.url + ': ' + console.error(error))
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(cacheFirstWithRefresh(event.request));
 });
